da54395de407e763c88c6c88cb1ec14f33102977
Роман Гаврилов <r.s.gavrilov@stud.mospolytech.ru>
2023-09-25 01:25:31 +0300

    isValidName for router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { HandlerCreateTrip, HandlerAddPassToTrip, HandlerFindAllTrips } from "../handlers/trips.js";
-import isValidData from "../midllewares/midlleware.js";
+import {isValidData, isValidName} from "../midllewares/midlleware.js";
 
 
 const routerVar = Router();
 
 routerVar.get("/trips", HandlerFindAllTrips);
 routerVar.post("/trips", isValidData, HandlerCreateTrip);
-routerVar.post("/trips/passengers", HandlerAddPassToTrip);
+routerVar.post("/trips/passengers", isValidName, HandlerAddPassToTrip);
 
 export default routerVar;
